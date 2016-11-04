16ba0ba0c70135be1771a44e907618e19267b936
danhamill <hamill.daniel@gmail.com>
2016-11-04 15:33:32 -0700

    inital commit

--- scripts/tl_var_comparisons_expanded.py
+++ scripts/tl_var_comparisons_expanded.py
@@ -14,18 +14,14 @@ import numpy as np
 from pandas.tools.plotting import table
 import sys
 
-def centeroidnp(df,query1,metric):
-    try:    
-        length = df.query(query1)['tl_'+ metric].dropna().values.size
-        sum_x = np.nansum(df.query(query1)['tl_'+ metric].values)
-    except:
-        length = df.query(query1)['tl_std'].dropna().values.size
-        sum_x = np.nansum(df.query(query1)['tl_std'].values)
-    sum_y = np.nansum(df.query(query1)['entropy_' +  metric].values)
+def centeroidnp(df,query1,metric): 
+    length = df.query(query1)['tl_'+ metric].dropna().values.size
+    sum_x = np.nansum(df.query(query1)['tl_'+ metric].values)
+    sum_y = np.nansum(df.query(query1)['var_' +  metric].values)
     return sum_x/length, sum_y/length
     
 def error_bars(df,query1,metric):
-    y_err = np.nanstd(df.query(query1)['entropy_' + metric].values)
+    y_err = np.nanstd(df.query(query1)['var_' + metric].values)
     try:    
         x_err = np.nanstd(df.query(query1)['tl_'+ metric].values)
     except:
@@ -42,7 +38,7 @@ ent_list = files[25:30]
 
 print "will save files to root directory %s" %(root,)
 
-n=4
+n=7
 #for n in xrange(len(ent_list)):
     
 tl = r"C:\workspace\texture_lengthscale\output\texture_lentghtscale_zonal_stats_merged.csv"
@@ -52,12 +48,12 @@ meter = ent.split('\\')[-1].split('_')[1]
 df1 = pd.read_csv(tl,sep=',')
 df2 = pd.read_csv(ent,sep=',')
 df1.rename(columns={'max':'tl_max', 'mean':'tl_mean', 'median':'tl_median','min':'tl_min','percentile_25':'tl_25','percentile_50':'tl_50', 'percentile_75':'tl_75','std':'tl_std'},inplace=True)   
-df2.rename(columns={'max':'entropy_max', 'mean':'entropy_mean', 'median':'entropy_median','min':'entropy_min','percentile_25':'entropy_25','percentile_50':'entropy_50', 'percentile_75':'entropy_75','std':'entropy_std'},inplace=True)
+df2.rename(columns={'max':'var_max', 'mean':'var_mean', 'median':'var_median','min':'var_min','percentile_25':'var_25','percentile_50':'var_50', 'percentile_75':'var_75','std':'var_std'},inplace=True)
 
 
 merge =df1.merge(df2,left_index=True, right_index=True, how='left')
 
-merge = merge[['tl_mean','entropy_mean','substrate_x','tl_25','entropy_25','tl_75','entropy_75']]
+merge = merge[['tl_mean','var_mean','substrate_x','tl_25','var_25','tl_75','var_75']]
 merge.rename(columns={'substrate_x':'substrate'},inplace=True)
 
 #Queries
@@ -91,15 +87,15 @@ green = mpatches.Patch(color='green',label='Gravel')
 red = mpatches.Patch(color='red',label='Boulders')
 
 
-oName = root + os.sep + "texture_lengthscale_entropy_comparison_expanded_" + meter +".png"   
+oName = root + os.sep + "texture_lengthscale_var_comparison_" + meter +".png"   
 fig,axes = plt.subplots(figsize =(8,6),nrows=3,ncols=2)
 
-merge.query('substrate == "sand"').plot.scatter(ax =axes[0,0], x='tl_25',y='entropy_25',color='blue')
+merge.query('substrate == "sand"').plot.scatter(ax =axes[0,0], x='tl_25',y='var_25',color='blue')
 cent_df.query('index == "sand"').plot.scatter(ax = axes[0,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='blue',s=100)
-merge.query('substrate == "gravel"').plot.scatter(ax =axes[0,0], x='tl_25',y='entropy_25',color='green')
+merge.query('substrate == "gravel"').plot.scatter(ax =axes[0,0], x='tl_25',y='var_25',color='green')
 cent_df.query('index == "gravel"').plot.scatter(ax = axes[0,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='green',s=100)
 cent_df.query('index == "boulders"').plot.scatter(ax = axes[0,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='red',s=100)
-merge.query('substrate == "boulders"').plot.scatter(ax =axes[0,0],x='tl_25',y='entropy_25',color='red')
+merge.query('substrate == "boulders"').plot.scatter(ax =axes[0,0],x='tl_25',y='var_25',color='red')
 axes[0,0].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, borderaxespad=0., handles=[blue,green,red], ncol=3, columnspacing=1, fontsize=8)
 
 axes[0,1].xaxis.set_visible(False)
@@ -123,12 +119,12 @@ cent_df.loc['sand'] = pd.Series({'x_cent':s_centroid[0] ,'y_cent':s_centroid[1] 
 cent_df.loc['gravel'] = pd.Series({'x_cent':g_centroid[0] ,'y_cent': g_centroid[1],'y_err':g_err[1] ,'x_err':g_err[0]})
 cent_df.loc['boulders'] = pd.Series({'x_cent':b_centroid[0] ,'y_cent':b_centroid[1] ,'y_err': b_err[1],'x_err':b_err[0]})
 cent_df = cent_df.reset_index()
-merge.query('substrate == "sand"').plot.scatter(ax =axes[1,0], x='tl_mean',y='entropy_mean',color='blue')
+merge.query('substrate == "sand"').plot.scatter(ax =axes[1,0], x='tl_mean',y='var_mean',color='blue')
 cent_df.query('index == "sand"').plot.scatter(ax = axes[1,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='blue',s=100)
-merge.query('substrate == "gravel"').plot.scatter(ax =axes[1,0], x='tl_mean',y='entropy_mean',color='green')
+merge.query('substrate == "gravel"').plot.scatter(ax =axes[1,0], x='tl_mean',y='var_mean',color='green')
 cent_df.query('index == "gravel"').plot.scatter(ax = axes[1,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='green',s=100)
 cent_df.query('index == "boulders"').plot.scatter(ax = axes[1,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='red',s=100)
-merge.query('substrate == "boulders"').plot.scatter(ax =axes[1,0],x='tl_mean',y='entropy_mean',color='red')
+merge.query('substrate == "boulders"').plot.scatter(ax =axes[1,0],x='tl_mean',y='var_mean',color='red')
 #axes[1,0].legend(loc=9,handles=[blue,green,red],ncol=3,columnspacing=1, fontsize=8)
 
 axes[1,1].xaxis.set_visible(False)
@@ -154,13 +150,13 @@ cent_df.loc['gravel'] = pd.Series({'x_cent':g_centroid[0] ,'y_cent': g_centroid[
 cent_df.loc['boulders'] = pd.Series({'x_cent':b_centroid[0] ,'y_cent':b_centroid[1] ,'y_err': b_err[1],'x_err':b_err[0]})
 
 cent_df = cent_df.reset_index()
-merge.query('substrate == "sand"').plot.scatter(ax =axes[2,0], x='tl_75',y='entropy_75',color='blue')
+merge.query('substrate == "sand"').plot.scatter(ax =axes[2,0], x='tl_75',y='var_75',color='blue')
 cent_df.query('index == "sand"').plot.scatter(ax = axes[2,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='blue',s=100)
 
-merge.query('substrate == "gravel"').plot.scatter(ax =axes[2,0], x='tl_75',y='entropy_75',color='green')
+merge.query('substrate == "gravel"').plot.scatter(ax =axes[2,0], x='tl_75',y='var_75',color='green')
 cent_df.query('index == "gravel"').plot.scatter(ax = axes[2,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='green',s=100)
 cent_df.query('index == "boulders"').plot.scatter(ax = axes[2,0], x='x_cent',y='y_cent', yerr= 'y_err', xerr='x_err',color='red',s=100)
-merge.query('substrate == "boulders"').plot.scatter(ax =axes[2,0],x='tl_75',y='entropy_75',color='red')
+merge.query('substrate == "boulders"').plot.scatter(ax =axes[2,0],x='tl_75',y='var_75',color='red')
 
 #axes[2,0].legend(loc=9,handles=[blue,green,red],ncol=3,columnspacing=1, fontsize=8)
 
